641f0a98f0857728b9991dc585536aa777141fff
jeongsseongg <jeongsseongg@gmail.com>
2026-08-01 15:59:37 +0900

    검색 개편 롤백 후 견적 모달 유지

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 벨로르 PWA 서비스워커 */
-const VERSION = "bellore-v197-mypick-reference-4";
+const VERSION = "bellore-v202-search-rollback";
 const SHELL_CACHE = `${VERSION}-shell`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 
